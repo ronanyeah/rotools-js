ef98123646f70d305ef2531e1bb20e6bcff4dd3c
ronanyeah <ronan_mccabe@hotmail.com>
2016-12-04 05:16:19 +0000

    Refactor + bugfix.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 'use strict'
 
-const { assoc, replace } = require('ramda')
-const { readdirSync }    = require('fs')
-const { resolve }        = require('path')
+const { curry, assoc, replace } = require('ramda')
+const { reduce }                = require('sanctuary')
+const { readdirSync }           = require('fs')
+const { resolve }               = require('path')
 
-const srcFolder = `${resolve(__dirname)}/src`
+const srcFolder = resolve(`${__dirname}/src`)
 
 module.exports =
-  readdirSync(srcFolder)
-  .reduce(
+  reduce(
     // Object -> String -> Object
-    (acc, file) =>
-      assoc(
-        replace('.js', '', file),
-        require(srcFolder),
-        acc
-      ),
-    {}
+    curry(
+      (acc, file) =>
+        assoc(
+          replace('.js', '', file),
+          require(`${srcFolder}/${file}`),
+          acc
+        )
+    ),
+    {},
+    readdirSync(srcFolder)
   )
